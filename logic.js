5958424777a704f0e9e1ae8596908825b1b7721b
Griffin <griffinlebeau2@gmail.com>
2022-06-20 17:35:26 -0500

    function organization

--- logic.js
+++ logic.js
@@ -1,44 +1,58 @@
+var timeAudit = moment().set();
+var nineAM = $("#nineEvents");
+var tenAM = $("#tenEvents");
+var elevenAM = $("#elevenEvents");
+var twelvePM = $("#twelveEvents");
+var onePM = $("#oneEvents");
+var twoPM = $("#twoEvents");
+var threePM = $("#threeEvents");
+var fourPM = $("#fourEvents");
+var fivePM = $("#fiveEvents");
 
 $(document).ready(function (){
     loadEvents;
     printEvents;
-    console.log(hourEvents);
 })
 
 //array holding hours and associated event data 
-var hourEvents = [];
-
-var hourDataObj = {
-    hour: hourRow,
-    event: hourEventInput,
-};
-
-hourRow = 
-
-var createRows = function() {
-    $(<)
-}
-
-//print loaded events 
-var printEvents = function(hourEvents){
-    for (var i = 0; i < hourEvents.length; i++) {
-        $(".row-p").text(hourEvents[i]);
-        var id = hourEvents[i].id
-        hourEvents[i].moment().set('hour', id)
+var events = [
+    {
+        hour: moment('09:00 AM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('10:00 AM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('11:00 AM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('12:00 PM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('01:00 PM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('02:00 PM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('03:00 PM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('04:00 PM', "hh:mm A"),
+        events: "",
+    },
+    {
+        hour: moment('05:00 PM', "hh:mm A"),
+        events: "",
     }
-}
-
-//save events to local storage
-var saveEvents = function() {
-    localStorage.setItem("hourEvents", JSON.stringify(hourEvents));
-}
-
-//load events from local storage
-var loadEvents = function() {
-    hourEvents = localStorage.getItem("hourEvents");
-    hourEvents = JSON.parse(hourEvents);
-    console.log(hourEvents);
-}
+]
 
 //display date in jumbotron
 var currentHour = moment().format('hh a')
@@ -46,13 +60,47 @@ var currentTime = moment().format('dddd MMMM Do YYYY');
 var jumbotronTime = $(".jumbotronTime");
 jumbotronTime.text(currentTime);
 
+//print loaded events from array to matching element 
+var printEvents = function(events){
+    for (var i = 0; i < events.length; i++) {
+        nineAM.text(events[0].events);
+        tenAM.text(events[1].events);
+        elevenAM.text(events[2].events);
+        twelvePM.text(events[3].events);
+        onePM.text(events[4].events);
+        twoPM.text(events[5].events);
+        threePM.text(events[6].events);
+        fourPM.text(events[6].events);
+        fivePM.text(events[7].events);
+    }
+}
+
+//save events to local storage
+var saveEvents = function() {
+    events[0].events = nineAM.text().trim();
+    events[1].events = tenAM.text().trim();
+    events[2].events = elevenAM.text().trim();
+    events[3].events = twelvePM.text().trim();
+    events[4].events = onePM.text().trim();
+    events[5].events = twoPM.text().trim();
+    events[6].events = threePM.text().trim();
+    events[7].events = fourPM.text().trim();
+    events[8].events = fivePM.text().trim();
+    localStorage.setItem("events", JSON.stringify(events));
+}
+
+//load events from local storage
+var loadEvents = function() {
+    events = localStorage.getItem("events");
+    events = JSON.parse(events);
+}
 //on click text area change to from p to input element
 $(".row").on("click", "p", function(){
     var text = $(this)
         .text()
         .trim();
     var textInput = $("<textarea>")
-        .addClass("form-control ")
+        .addClass("form-control")
         .val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
@@ -63,35 +111,32 @@ $(".row").on("blur", "textarea", function(){
     var text = $(this)
         .val()
         .trim();
-    var hour = $(this)
-        .closest(".row")
-        .attr("id");
-    console.log(hour);
     var textEvent = $("<p>")
         .text(text);
     $(this).replaceWith(textEvent);
-    saveEvents;
+    saveEvents();
 })
 
-var timeNine = moment().set('hour', 9);
-
-//audit blocks
-var auditNine = function () {
-    if (currentHour.isBefore(timeNine)) {
-        $("#nineAm")
-            .removeClass(".future .present")
-            .addClass(".past");
-    }
-    else if (currentHour.isAfter(timeNine)) {
-        $("#nineAm")
-            .removeClass(".past .present")
-            .addClass(".future");
-    }
-    else {
-        $("#nineAm")
-            .removeClass(".past .future")
-            .addClass(".present");
+//audit blocks using set moment hour in array of events 
+var auditTime = function () {
+    for (var i = 0; i < events.length; i++) {
+        if (events[i].hour.isBefore(timeAudit)) {
+            $(".row")
+                .addClass(".past")
+                .removeClass(".future .present");
+        }
+        else if (events[i].hour.isAfter(timeAudit)) {
+            $(".row")
+                .addClass(".future")
+                .removeClass(".past .present");
+        }
+        else {
+            $(".row")
+                .addClass(".present")
+                .removeClass(".past .future");
+        }
     }
 }
 
+setInterval(auditTime, 100000);
 
